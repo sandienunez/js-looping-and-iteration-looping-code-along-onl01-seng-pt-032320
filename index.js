a301c7ff0e62ef3778ab77fa7622100d307698c0
github username <github email address>
2020-10-06 14:56:05 -0400

    Done.

--- index.js
+++ index.js
@@ -3,10 +3,27 @@
 // [loop body]
 // }
 
-function writeCards( names, events) {
-    for (let people = 3; people < names.length; people++) {
-      console.log(`Thank you, ${names}, for the wonderful ${events} gift!`);
-    }
-    return names;
-}
+// function writeCards( names, events) {
+//     for (let people = 3; people < names.length; people++) {
+//       console.log(`Thank you, ${names}, for the wonderful ${events} gift!`);
+//     }
+//     return names;
+// }
  
+function writeCards( names, events) {
+  const newArray = []
+ 
+    for (let counter = 0; counter < names.length; counter++) {
+     newArray.push(`Thank you, ${names[counter]}, for the wonderful ${events} gift!`);
+
+      // '.push' correct message/info into newArray on this line
+    }
+        
+    return newArray
+  }
+
+  function countDown() {
+  let i = 10;
+while (i >= 0) 
+  console.log(i--);
+}
