6cc340c25d1bfe26d5986b426182e434dd7aa0c7
github username <github email address>
2020-10-06 09:29:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+// for ([initialization]; [condition]; [iteration]) {
+// [loop body]
+// }
+
+function writeCards( names, events) {
+    for (let people = 3; people < names.length; people++) {
+      console.log(`Thank you, ${names}, for the wonderful ${events} gift!`);
+    }
+    return names;
+}
+ 
